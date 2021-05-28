c39c4f0bf9a1cf05308da65bb2a9c199e86ce3ba
anton.uspehov <32952745+antonuspehov@users.noreply.github.com>
2021-05-28 12:28:29 +0300

    Configure Stack parameters (name, tags, env)

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 
+import datetime
+
 from aws_cdk import core as cdk
 from rialto_bus_bot.rialto_bus_bot_stack import RialtoBusBotStack
 
 
+stack_name = "rialto-bus-bot"
+stack_tags = {
+    "project": stack_name,
+    "cdk": "true",
+    "updated_at": datetime.datetime.utcnow().isoformat(),
+}
+stack_env = {"region": "eu-north-1"}
+
 app = cdk.App()
-RialtoBusBotStack(app, "rialto-bus-bot", env={"region": "eu-north-1"})
+RialtoBusBotStack(app, stack_name, tags=stack_tags, env=stack_env)
 
 app.synth()
