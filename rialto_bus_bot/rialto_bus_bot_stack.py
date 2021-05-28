170f9f1a00eac9d4b298124df86324f44ffe73df
anton.uspehov <32952745+antonuspehov@users.noreply.github.com>
2021-05-28 12:27:55 +0300

    Add first handler (bot.handler) for initial deploy

---
+++ rialto_bus_bot/rialto_bus_bot_stack.py
@@ -0,0 +1,19 @@
+from aws_cdk import (
+    core as cdk,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw
+)
+
+
+class RialtoBusBotStack(cdk.Stack):
+
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+    lambda_function = _lambda.Function(
+        self,
+        "BotHandler",
+        runtime=_lambda.Runtime.PYTHON_3_8,
+        code=_lambda.Code.from_asset("lambda"),
+        handler="bot.handler"
+    )
