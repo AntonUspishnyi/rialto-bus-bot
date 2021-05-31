f1a016adddfcc87bb18c672ef04fb00dbfaf3ddd
anton.uspehov <32952745+antonuspehov@users.noreply.github.com>
2021-05-31 19:56:34 +0300

    Add python-dotenv to simplify local development

--- app.py
+++ app.py
@@ -3,14 +3,18 @@
 import datetime
 
 from aws_cdk import core as cdk
+from dotenv import load_dotenv
 from rialto_bus_bot.rialto_bus_bot_stack import RialtoBusBotStack
 
+# Load env from .env to simplify local development
+load_dotenv()
 
+# Stack parameters
 stack_name = "rialto-bus-bot"
 stack_tags = {
     "project": stack_name,
     "cdk": "true",
-    "updated_at": datetime.datetime.utcnow().isoformat(),
+    "last_deploy_at": datetime.datetime.utcnow().isoformat(),
 }
 stack_env = {"region": "eu-north-1"}
 
