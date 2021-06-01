c05ca185884626db1e10bb1bc066ef32926ec02d
Anton Lipovskih <anton.lipovskih@wikrgroup.com>
2021-06-01 16:00:51 +0300

    Remove timestamp from tags to avoid resource modification on each deploy + remove stack tags

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-import datetime
-
 from aws_cdk import core as cdk
 from dotenv import load_dotenv
 from rialto_bus_bot.rialto_bus_bot_stack import RialtoBusBotStack
@@ -14,11 +12,9 @@ stack_name = "rialto-bus-bot"
 stack_tags = {
     "project": stack_name,
     "cdk": "true",
-    "last_deploy_at": datetime.datetime.utcnow().isoformat(),
 }
-stack_env = {"region": "eu-north-1"}
 
 app = cdk.App()
-RialtoBusBotStack(app, stack_name, tags=stack_tags, env=stack_env)
+RialtoBusBotStack(app, stack_name, tags=stack_tags)
 
 app.synth()
