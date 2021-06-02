c74ce7f9adeae1dda33d0a0a17c81161dd96c695
anton.uspehov <32952745+antonuspehov@users.noreply.github.com>
2021-06-02 17:02:17 +0300

    Add functionality to get a schedule for any day in schedule.yaml

--- lambda/bot.py
+++ lambda/bot.py
@@ -1,10 +1,17 @@
+import datetime
 import logging
 import os
 
+import pytz
 import telebot
+import yaml
 from telebot import types
 
 bot = telebot.TeleBot(os.environ["TG_BOT_TOKEN"], threaded=False)
+
+# Define the right timezone for bot calculations
+tz = pytz.timezone("Europe/Kiev")
+now = datetime.datetime.now(tz)
 
 # Define buttons for main markup
 button_next_bus = "When's the next bus?"
@@ -26,30 +33,34 @@ def handler(event, context) -> dict:
 
 
 # Handle "/start" and "/help" commands
-@bot.message_handler(commands=["start", "help"])
+@bot.message_handler(commands=["start"])
 def send_welcome(message) -> None:
     bot.send_message(
         message.chat.id,
-        f"Hi {get_username(message.from_user)}, here are the available commands:",
+        f"Hi {get_username(message.from_user)}, here are the available commands👇",
         reply_markup=main_markup(),
     )
 
 
+# Reply when's the next bus
 @bot.message_handler(func=lambda message: message.text == button_next_bus)
-def send_welcome(message) -> None:
+def send_next_bus(message) -> None:
     bot.reply_to(message, f"You hit the {button_next_bus} button", reply_markup=main_markup())
 
 
+# Reply with schedule for today or specified day
 @bot.message_handler(func=lambda message: message.text == button_today_schedule)
-def send_welcome(message) -> None:
-    bot.reply_to(message, f"You hit the {button_today_schedule} button", reply_markup=main_markup())
+@bot.message_handler(func=lambda message: message.text in [day for day in load_schedule()])
+def send_day_schedule(message) -> None:
+    day = get_weekday(now) if message.text == button_today_schedule else message.text
+    schedule = format_schedule(day, get_schedule_for(day))
+    bot.send_message(message.chat.id, schedule, parse_mode="HTML", reply_markup=main_markup())
 
 
+# Reply with markup with all days from schedule
 @bot.message_handler(func=lambda message: message.text == button_complete_schedule)
 def send_welcome(message) -> None:
-    bot.reply_to(
-        message, f"You hit the {button_complete_schedule} button", reply_markup=main_markup()
-    )
+    bot.reply_to(message, f"Choose the day:", reply_markup=schedule_markup())
 
 
 # Handle all other messages
@@ -57,7 +68,7 @@ def send_welcome(message) -> None:
 def send_re_ask(message) -> None:
     bot.reply_to(
         message,
-        f"Sorry, {get_username(message.from_user)}, I don't understand you. Here are the available commands:",
+        f"Sorry, {get_username(message.from_user)}, I don't understand you 🤔\nCheck the available buttons.",
         reply_markup=main_markup(),
     )
 
@@ -73,5 +84,40 @@ def main_markup() -> types.ReplyKeyboardMarkup:
     return build_markup([button_next_bus, button_today_schedule, button_complete_schedule])
 
 
+def schedule_markup() -> types.ReplyKeyboardMarkup:
+    return build_markup([day for day in load_schedule()])
+
+
 def get_username(from_user: types.User) -> str:
     return from_user.first_name or from_user.username
+
+
+def load_schedule(path: str = "./schedule.yaml") -> dict:
+    with open(path) as file:
+        return yaml.safe_load(file)
+
+
+def get_weekday(dt: datetime.datetime) -> str:
+    return dt.strftime("%A")
+
+
+def get_schedule_for(weekday: str) -> dict:
+    schedule = load_schedule().get(weekday, {})
+
+    # Swap time and description for future formatting
+    schedule_swapped = {}
+    for time, description in schedule.items():
+        schedule_swapped.setdefault(description, []).append(time)
+    return schedule_swapped
+
+
+def format_schedule(weekday: str, schedule: dict) -> str:
+    if not schedule:
+        return f"Could not find schedule for {weekday} ☹️"
+
+    schedule_list = [f"<b>{weekday} schedule:</b>"]
+    for description, time_list in schedule.items():
+        schedule_list.append(f"\n{description}")
+        time_list.sort(key=lambda t: datetime.time.fromisoformat(t))
+        schedule_list.append("\n".join(f"🔹<code>{t}</code>" for t in time_list))
+    return "\n".join(schedule_list)
